0e6f21a76d0f914abf503b0453aa55e9dbd82c83
Zach Sents <zachsents@gmail.com>
2026-07-17 12:20:20 -0600

    load prettier plugin module and bump configs

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,6 +1,8 @@
+import * as prettierPluginJsdoc from "prettier-plugin-jsdoc"
+
 export default {
   semi: false,
   tabWidth: 2,
   useTabs: false,
-  plugins: ["prettier-plugin-jsdoc"],
+  plugins: [prettierPluginJsdoc],
 }
